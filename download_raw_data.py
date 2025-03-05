eb977b622415b9dbe511f33c1ae202d91131451d
PolinaM <76835109+PolinaMatv@users.noreply.github.com>
2025-03-05 11:30:39 -0600

    Add files via upload

---
+++ download_raw_data.py
@@ -0,0 +1,20 @@
+import subprocess
+
+with open("healpx_to_download.txt", "r") as file:
+    lines = file.readlines()
+
+to_download = [line.strip() for line in lines]
+
+# download some data using bash scripts
+wget_path = '/opt/homebrew/bin/wget'
+
+
+def download_healpix(h):
+    bash_script = (
+        f"{wget_path} -r -np -nH --cut-dirs=1 -R 'index.html*' -P ~/Desktop/Galaxy_Classification/spectrum_data "
+        f"https://users.flatironinstitute.org/~polymathic/data/MultimodalUniverse/v1/sdss/sdss/healpix={h}/"
+    )
+    subprocess.run(bash_script, shell=True, capture_output=True, text=True)
+
+    for healpx in to_download:
+        download_healpix(healpx)
